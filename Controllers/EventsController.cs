b60b0c7149d51081115f54de22dcfbf2c097e3c6
Samybeaudoin <154572064+Samybeaudoin@users.noreply.github.com>
2025-11-29 23:31:50 -0500

    Ajoutdel'actionPrecise

--- Controllers/EventsController.cs
+++ Controllers/EventsController.cs
@@ -10,7 +10,7 @@ using Backend.Models;
 
 namespace Backend.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[Action]")]
     [ApiController]
     public class EventsController : ControllerBase
     {
